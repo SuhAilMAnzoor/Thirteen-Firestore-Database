ab0e1202baccb704d968380d45baf4f23f30afdd
SuhAilMAnzoor <sohailmanzoorsario@gmail.com>
2025-08-04 15:59:27 +0500

    Add login screen with Email/password using Firebase Auth, with input validation, error handling, and user navigation to user profile

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,139 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:thirteen_firestore_database/screens/getUserName.dart';
+import 'package:thirteen_firestore_database/screens/registerscreen.dart';
+import '../utill/ElevatedButtonColors.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // TextField Controllers for Email and Password
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isLogin = false;
+
+  // Login Function
+  void loginFunction() async {
+    if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Please fill out all fields."),
+          backgroundColor: Colors.orange,
+        ),
+      );
+      return;
+    }
+
+    try {
+      setState(() {
+        isLogin = true; // Show loading indicator
+      });
+
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text,
+      );
+      // After successful login, get the UID of the logged-in user
+      String getUserLoginUID = FirebaseAuth.instance.currentUser!.uid;
+
+      // If Successful, Navigate to the User Page
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => GetUserName(documentId: getUserLoginUID)),
+      );
+    } catch (e) {
+      setState(() {
+        isLogin = false;
+      });
+
+      String message;
+      if (e is FirebaseAuthException) {
+        if (e.code == 'user-not-found') {
+          message = 'No user found for that email.';
+        } else if (e.code == 'wrong-password') {
+          message = 'Wrong password provided.';
+        } else {
+          message = "An error occurred. Please try again.";
+        }
+      } else {
+        message = "An unexpected error occurred.";
+      }
+
+      // Display Error Message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login Screen")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
+                hintText: "Type your email",
+                hintStyle: const TextStyle(fontSize: 12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                hintText: "Type your password",
+                hintStyle: const TextStyle(fontSize: 12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: loginFunction,
+              style: ButtonStyles.primaryButtonStyle, // Custom Button Style
+              child: const Text("Login"),
+            ),
+            const SizedBox(height: 15),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const RegistrationScreen()),
+                );
+              },
+              child: const Text("Don't have an account? Register"),
+            ),
+            const SizedBox(height: 10),
+            Visibility(
+              visible: isLogin,
+              child: const Center(child: CircularProgressIndicator()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
